3dff85b0898dd73da79bbe75ba1ef0f8a62170a1
akb0356 <magic081435@gmail.com>
2023-11-01 00:52:41 +0900

    20231031

--- 231031/09js/script.js
+++ 231031/09js/script.js
@@ -1,5 +1,17 @@
-let number1 = prompt("1숫자 주세요")
-let number2 = prompt("2숫자 주세요")
-let number3 = prompt("3숫자 주세요")
+let a = parseInt(prompt("첫번째 변"));
+let b = parseInt(prompt("두번째 변"));
+let c = parseInt(prompt("세번째 변"));
 
+let numbers = new Array(a, b, c);
+numbers.sort((a, b) => {
+  return a - b;
+});
 
+let compare = (numbers) => {
+  if (numbers[0] + numbers[1] > numbers[2]) {
+    alert(`삼각형을 만들 수 있습니다.`);
+  }
+  alert(`삼각형을 만들 수 없습니다.`);
+};
+
+compare(numbers);
