afacdadfc0cffd425a86c163f3d91b368b18183e
akb0356 <magic081435@gmail.com>
2024-01-05 02:21:09 +0900

    20240104

---
+++ portfolio/portfolio/public/clonecoding/05.museum/script.js
@@ -0,0 +1,53 @@
+// mainSlider
+const slideWrapper = document.querySelector(".mainSlide");
+const sliderContainer = document.querySelector(".mainSlide-wrapper");
+const slides = sliderContainer.querySelectorAll(".main-slide");
+const slideCounts = slides.length;
+const pager = document.querySelector(".mainSlide-pagination-span");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const slideBtn = pager.querySelector("button");
+let currentIndex = 0;
+let pagerHTML = "";
+
+for (let i = 0; i < slideCounts; i++) {
+  slides[i].style.left = `${i * 100}%`;
+  pagerHTML += `<span data-idx="${i}">${i + 1}</span>`;
+  pager.innerHTML = pagerHTML;
+}
+
+const pagerBtn = pager.querySelectorAll("span");
+
+const goToslide = (i) => {
+  sliderContainer.classList.add("animated");
+  currentIndex = i;
+  sliderContainer.style.left = `${i * -100}%`;
+  for (let i = 0; i < pagerBtn.length; i++) {
+    pagerBtn[i].classList.remove("active");
+  }
+  pagerBtn[i].classList.add("active");
+};
+
+prevBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentIndex > 0) {
+    goToslide(currentIndex - 1);
+  } else {
+    goToslide(slideCounts - 1);
+  }
+});
+nextBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentIndex < slideCounts - 1) {
+    goToslide(currentIndex + 1);
+  } else {
+    goToslide(0);
+  }
+});
+
+for (let i = 0; i < pagerBtn.length; i++) {
+  pagerBtn[i].addEventListener("click", (e) => {
+    let pagerNum = e.target.innerText - 1;
+    goToslide(pagerNum);
+  });
+}
---
+++ portfolio/portfolio/public/script.js
@@ -0,0 +1,26 @@
+//header
+const header = document.querySelector("header");
+const blackBox = document.querySelector(".blackBox");
+const mainContentWrap = document.querySelector(".mainContentWrap");
+const scrolleffect = document.querySelector(".scrolleffect");
+
+let scrollNum = 0;
+let documentHeight = 0;
+const coverHeight = window.innerHeight;
+
+window.addEventListener("scroll", () => {
+  scrollNum = window.scrollY;
+  documentHeight = document.body.scrollHeight;
+  if (scrollNum >= coverHeight) {
+    header.classList.add("fixed");
+    scrolleffect.style.opacity = `${scrollNum / 1000}`;
+  } else {
+    header.classList.remove("fixed");
+    blackBox.style.backgroundColor = `rgba(0, 0, 0, ${scrollNum / 1000})`;
+    blackBox.style.zIndex = `${scrollNum / 1000}`;
+    scrolleffect.style.opacity = `${1 - scrollNum / 1000}`;
+  }
+});
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
